53e8d9171c100f4efe7e31cd55d6d5533684c13f
Tejas <tejas14all@gmail.com>
2021-06-26 15:49:15 +0530

    update delete responsebean

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -3,11 +3,15 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bean.EmployeeBean;
+import com.bean.ResponseBean;
 import com.dao.EmployeeDao;
 
 @RestController
@@ -15,17 +19,71 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeDao employeeDao;
-	
+
 	@PostMapping("/employee")
-	public EmployeeBean saveEmployee(EmployeeBean employee) {
-		
+	public ResponseBean<EmployeeBean> saveEmployee(EmployeeBean employee) {
+
 		employeeDao.insertEmployee(employee);
-		return employee;
+		return ResponseBean.data(employee, "Employee Save", 200);
 	}
-	
+
 	@GetMapping("/employees")
-	public List<EmployeeBean> getAllEmps(){
-		return  employeeDao.getAllEmployees();
+	public ResponseBean<List<EmployeeBean>> getAllEmps() {
+		return ResponseBean.data(employeeDao.getAllEmployees(), "Employees Retrieved", 200);
 	}
-	
+
+//	@GetMapping("/employee/{empId}")
+//	public ResponseBean<EmployeeBean> getEmployeeById(@PathVariable("empId") int empId) {
+//		EmployeeBean employee = employeeDao.getEmployeeById(empId);
+//
+//		ResponseBean<EmployeeBean> rb = new ResponseBean<>();
+//
+//		if (employee == null) {
+//			rb.setMsg("Invalid employee Id");
+//			rb.setStatus(-1);
+//			
+//		
+//		} else {
+//			rb.setData(employee);
+//			rb.setStatus(200);
+//			rb.setMsg("Employee retrieved");
+//
+//		}
+//		return rb;
+//	}
+
+	@GetMapping("/employee/{empId}")
+	public ResponseBean<EmployeeBean> getEmployeeById(@PathVariable("empId") int empId) {
+		EmployeeBean employee = employeeDao.getEmployeeById(empId);
+
+		if (employee == null) {
+
+			return ResponseBean.data(employee, "Invalid EmployeeId", -1);
+
+		} else {
+			return ResponseBean.data(employee, "Employee Successfully retrieved", 200);
+
+		}
+	}
+
+	@DeleteMapping("/employee/{empId}")
+	public ResponseBean deleteEmployeeById(@PathVariable("empId") int empId) {
+
+		if (employeeDao.deleteEmployeeById(empId) == true) {
+			return ResponseBean.data(null, "Employee Deleted", 200);
+		} else {
+			return ResponseBean.data(null, "Invalid Employee Id", -1);
+		}
+	}
+
+	@PutMapping("/employee")
+	public ResponseBean<EmployeeBean> updateEmployee(EmployeeBean employee) {
+		System.out.println(employee.getEmpId());
+		if (employeeDao.updateEmployee(employee) != 0) {
+			return ResponseBean.data(employee, "Employee updated", 200);
+		}else {
+			return ResponseBean.data(employee, "Invalid Data", -1);
+		}
+	}
+
 }
--- src/main/java/com/dao/EmployeeDao.java
+++ src/main/java/com/dao/EmployeeDao.java
@@ -17,13 +17,39 @@ public class EmployeeDao {
 	public void insertEmployee(EmployeeBean empBean) {
 		stmt.update("insert into employee (ename,salary) values (?,?)", empBean.getEname(), empBean.getSalary());
 	}
-	
-	
-	
-	public List<EmployeeBean> getAllEmployees(){
+
+	public List<EmployeeBean> getAllEmployees() {
 		return stmt.query("select * from employee", new BeanPropertyRowMapper<EmployeeBean>(EmployeeBean.class));
 	}
-	
-	
-	
+
+	public EmployeeBean getEmployeeById(int empId) {
+
+		EmployeeBean employee = null;
+
+		try {
+			employee = stmt.queryForObject("select * from employee where empid  = ?", new Object[] { empId },
+					new BeanPropertyRowMapper<EmployeeBean>(EmployeeBean.class));
+		} catch (Exception e) {
+
+		}
+		return employee;
+	}
+
+	public boolean deleteEmployeeById(int empId) {
+
+		stmt.update("delete from empskill where empid = ?", empId);
+
+		int i = stmt.update("delete from employee where empid = ?", empId);
+		if (i == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public int updateEmployee(EmployeeBean employee) {
+		return stmt.update("update employee set ename = ? , salary = ? where  empid = ?", employee.getEname(),
+				employee.getSalary(), employee.getEmpId());
+	}
+
 }
