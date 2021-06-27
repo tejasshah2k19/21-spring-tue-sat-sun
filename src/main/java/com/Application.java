ac1a305d65e2febb0f7b8c94836212baf3e397bd
Tejas <tejas14all@gmail.com>
2021-06-27 21:36:27 +0530

    swagger 2

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,12 +2,36 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Docket myApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com"))
+
+				.build()
+				.apiInfo(apiInfo())
+				;
+	}
+
+	private  ApiInfo apiInfo() {
+	
+		ApiInfo ap = new ApiInfo("21-spring-tue-sat-sun", "dummy rest api", "1", "www.tos.com","Tejas Shah", "Apache 2.0", "www.apache.com");
+		return ap;
 	}
 
 }
